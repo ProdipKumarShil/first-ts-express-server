a8294d60e3bff0851be356e95c793b8795c36667
Prodip Kumar Shil <prodipkrishna01@gmail.com>
2024-08-15 00:33:20 +0600

    initial setup

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import config from "./app/config";
+import app from "./app";
+
+async function main() {
+  try {
+    await mongoose.connect(config.db_url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.db_url}`);
+    });
+  } catch (error) {
+    console.log(error)
+  }
+}
+// mongodb://localhost:27017
